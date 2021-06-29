578ff1214bf770c07312114be0bbd73f09a3ac98
Josef Wiklund <josef@qualitythink.se>
2021-06-29 14:50:56 +0200

    Better setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 export $(grep -v '^#' .env | xargs)
 
+if [ -z "$PROJECT_NAME" ]
+then
+echo "You need to setup the .env file"
+exit
+fi
+
+clear
+echo "Setting up ${PROJECT_NAME}"
+sleep .5
+clear
+echo "Setting up ${PROJECT_NAME}."
+sleep .5
+clear
+echo "Setting up ${PROJECT_NAME}.."
+sleep .5
+clear
+echo "Setting up ${PROJECT_NAME}..."
+sleep .5
+clear
+
+echo "Creating source folder"
 mkdir src
 
 bash apache-setup.sh
 
+echo "Creating docker containers"
 docker-compose up -d --build
 
+echo "Setting up laravel"
 docker exec ${PROJECT_NAME}-php-apache composer create-project laravel/laravel .
 docker exec ${PROJECT_NAME}-php-apache chmod -R 777 ./storage
