9e3664123ebbc9f60100511847608f8257a997f4
Josef Wiklund <josef@qualitythink.se>
2021-06-29 14:22:47 +0200

    Added apache setup to setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ export $(grep -v '^#' .env | xargs)
 
 mkdir src
 
+bash apache-setup.sh
+
 docker-compose up -d --build
 
 docker exec ${PROJECT_NAME}-php-apache composer create-project laravel/laravel .
